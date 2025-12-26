4f9378e38d8ca272f47546f7f96330cc4856184b
Piyush Makwana <student.piyushmakwana@gmail.com>
2025-12-26 16:35:54 +0530

    feat: Add Procfile for web process and configure app to bind to 0.0.0.0 using environment-defined port.

--- run.py
+++ run.py
@@ -30,8 +30,8 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(
-        host="127.0.0.1",
-        port=5000,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
         debug=config.DEBUG,
         use_reloader=True
     )
